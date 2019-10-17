a6f8749587c3b35064777782cda3b601cf8ee433
Pernat1y <mirnesen@gmail.com>
2019-10-17 12:47:31 +0300

    Display connection status

--- enable_data.sh
+++ enable_data.sh
@@ -13,12 +13,12 @@
 #
 
 # Check for dependencies
-if ! which curl &>/dev/null; then
-    echo 'I need curl to work. Exiting.'
+if ! command -v curl &>/dev/null; then
+    echo 'I need curl to work. Exiting'
     exit 1
 fi
-if ! which usb_modeswitch &>/dev/null; then
-    echo 'You may need usb_modeswitch to work properly.'
+if ! command -v usb_modeswitch &>/dev/null; then
+    echo 'You may need usb_modeswitch to work properly'
 fi
 
 # Check for modem
@@ -27,7 +27,7 @@ if lsusb | grep -e '12d1:14dc' -e '12d1:1f01' &>/dev/null; then
     # 12d1:1f01 Huawei Technologies Co., Ltd. E353/E3131 (Mass storage mode)
     echo 'Modem found.'
 else
-    echo 'Modem not found. Exiting.'
+    echo 'Modem not found. Exiting'
     exit 1
 fi
 
@@ -38,7 +38,7 @@ usb_modeswitch --default-vendor 12d1 --default-product 14dc --huawei-new-mode &>
 if curl "http://192.168.8.1" --silent &>/dev/null; then
     echo 'Connection to web interface successful'
 else
-    echo 'Unable to connect to web interface. Exiting.'
+    echo 'Unable to connect to web interface. Exiting'
     exit 1
 fi
 
@@ -64,7 +64,8 @@ curl "http://192.168.8.1/api/dialup/mobile-dataswitch" \
      --data '<?xml version="1.0" encoding="UTF-8"?><request><dataswitch>1</dataswitch></request>' \
      --compressed \
      --insecure \
-     --silent
+     --silent \
+     --output /dev/null
 curl "http://192.168.8.1/api/dialup/mobile-dataswitch" \
      -H "Cookie: SessionID=$SessionID" \
      -H 'DNT: 1' \
@@ -77,14 +78,15 @@ curl "http://192.168.8.1/api/dialup/mobile-dataswitch" \
      -H 'Connection: keep-alive' \
      --compressed \
      --insecure \
-     --silent
+     --silent \
+     --output /dev/null
 
 # Check status
-# <ConnectionStatus>900</ConnectionStatus> - connecting
-# <ConnectionStatus>901</ConnectionStatus> - connected
-# <ConnectionStatus>902</ConnectionStatus> - disconnected
-# <ConnectionStatus>903</ConnectionStatus> - disconnecting
-curl "http://192.168.8.1/api/monitoring/status" \
+# <ConnectionStatus>900</ConnectionStatus> - Connecting
+# <ConnectionStatus>901</ConnectionStatus> - Connected
+# <ConnectionStatus>902</ConnectionStatus> - Disconnected
+# <ConnectionStatus>903</ConnectionStatus> - Disconnecting
+status=$(curl "http://192.168.8.1/api/monitoring/status" \
      -H "Cookie: SessionID=$SessionID" \
      -H 'DNT: 1' \
      -H 'Accept-Encoding: gzip, deflate' \
@@ -96,5 +98,24 @@ curl "http://192.168.8.1/api/monitoring/status" \
      -H 'Connection: keep-alive' \
      --compressed \
      --insecure \
-     --silent
+     --silent |\
+     grep '<ConnectionStatus>' | cut -b 19-21)
+if [[ $status -eq 900 ]]; then
+    echo 'Status 900 (Connecting)'
+elif [[ $status -eq 901 ]]; then
+    echo 'Status 901 (Connected)'
+elif [[ $status -eq 901 ]]; then
+    echo 'Status 902 (Disconnected)'
+elif [[ $status -eq 901 ]]; then
+    echo 'Status 903 (Disconnecting)'
+else
+    echo 'Unknown status'
+fi
+
+# Test captive portal (catches all DNS requests)
+if ping -c 1 domain.nonexisted &>/dev/null; then
+    echo 'Captive portal enabled. Try to rerun script'
+else
+    echo 'Captive portal disabled (OK)'
+fi
 
